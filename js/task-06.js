913a0f00afa6c22fdd90fc8c66dd8a5ba39a2f4c
ribacot <remontdiskov1979@gmail.com>
2023-05-20 17:20:53 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,15 @@
 const inputEl = document.querySelector("#validation-input");
 const inputElLength = Number(inputEl.dataset.length);
-const hasValid = (event) => {
+
+const handlHasValidForm = (event) => {
 	const evt = event.currentTarget;
 	if (evt.value.length < inputElLength) {
+		evt.classList.remove("valid");
 		evt.classList.add("invalid");
 		return;
 	}
+	evt.classList.remove("invalid");
 	evt.classList.add("valid");
 };
-inputEl.addEventListener("blur", hasValid);
+
+inputEl.addEventListener("blur", handlHasValidForm);
